f54e4a7b637aa2ba79293c56d8e941d0bbd76e8e
Grant Holle <hollegrant@gmail.com>
2021-03-15 21:55:31 +0800

    Set up some tailwind properties

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,40 @@
+const colors = require('tailwindcss/colors')
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
-  darkMode: false,
+  darkMode: 'class',
   theme: {
     container: {
       center: true,
-      padding: '2rem',
+      padding: '1rem',
     },
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: [
+          'Inter',
+          ...defaultTheme.fontFamily.sans,
+        ]
+      },
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      primary: colors.blue,
+      black: colors.black,
+      white: colors.white,
+      gray: colors.trueGray,
+      red: colors.red,
+      yellow: colors.yellow,
+      blue: colors.blue,
+      green: colors.green,
+      orange: colors.orange,
+    }
   },
   variants: {
     extend: {},
   },
   plugins: [
     require('@tailwindcss/forms'),
-    require('tailwindcss-debug-screens'),
   ],
   purge: [
     './src/**/*.js',
