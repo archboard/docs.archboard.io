7ebd587ab2420984b697b9c002e43a7668357ce8
Grant Holle <hollegrant@gmail.com>
2021-03-17 22:16:12 +0800

    Add separate account layout and begin writing docs for the account management application

---
+++ src/_data/accountNavigation.js
@@ -0,0 +1,8 @@
+module.exports = [
+  { name: 'Introduction', route: '' },
+  { name: 'Signing up', route: 'signing-up/' },
+  { name: 'Logging in', route: 'logging-in/' },
+  { name: 'Dashboard', route: 'logging-in/' },
+  { name: 'Products', route: 'logging-in/' },
+  { name: 'Subscriptions', route: 'logging-in/' },
+]
---
+++ src/en/account/signing-up.md
@@ -0,0 +1,8 @@
+---
+title: Signing up for your Archboard account
+layout: account/layout.njk
+---
+
+# Signing up for an Archboard account
+
+To sign up for your Archboard account, head on over to the <a href="https://account.archboard.io/register" target="_blank">registration page</a>.
