d6307823b43319f75661e855872d365fa268f4c7
Grant Holle <hollegrant@gmail.com>
2021-03-26 22:07:05 +0800

    Add tidal ptc starter

---
+++ src/_data/tidalNavigation.js
@@ -0,0 +1,22 @@
+module.exports = [
+  {
+    header: '',
+    items: [
+      { name: 'Introduction', route: '' },
+      { name: 'Signing up', route: 'signing-up/' },
+      { name: 'Dashboard', route: 'dashboard/' },
+      { name: 'Products', route: 'logging-in/' },
+      { name: 'Subscriptions', route: 'logging-in/' },
+    ],
+  },
+  {
+    header: 'Admin',
+    items: [
+      { name: 'Introduction', route: '' },
+      { name: 'Signing up', route: 'signing-up/' },
+      { name: 'Dashboard', route: 'dashboard/' },
+      { name: 'Products', route: 'logging-in/' },
+      { name: 'Subscriptions', route: 'logging-in/' },
+    ]
+  }
+]
---
+++ src/en/tidal-ptc/index.md
@@ -0,0 +1,5 @@
+---
+layout: tidal-ptc/layout.njk
+title: Tidal PTC
+description: Learn how to use Tidal PTC.
+---
