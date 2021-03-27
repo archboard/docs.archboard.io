05adb434cd7f08b254a9152cf676f3d619080b6b
Grant Holle <hollegrant@gmail.com>
2021-03-27 21:52:06 +0800

    Add teacher dashboard page

--- src/_data/tidalNavigation.js
+++ src/_data/tidalNavigation.js
@@ -10,7 +10,7 @@ module.exports = [
     header: 'Admins and Teachers',
     items: [
       { name: 'Settings', route: 'admin/settings/' },
-      { name: 'Signing up', route: 'signing-up/' },
+      { name: 'Dashboard', route: 'admin/dashboard/' },
       { name: 'Dashboard', route: 'dashboard/' },
       { name: 'Products', route: 'logging-in/' },
       { name: 'Subscriptions', route: 'logging-in/' },
